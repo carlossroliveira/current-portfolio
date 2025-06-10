097f4e78c003c7339f600572b879a4f64b65585a
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-06-10 12:09:38 -0300

    Remove Header component from HomePage and adjust layout for improved clarity

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,12 @@
+import Divider from '../components/Divider'
 import Banner from '../sections/banner'
-import Header from '../sections/header'
 
 export default function HomePage() {
   return (
     <div className="bg-gray-700 h-screen px-4">
-      <Header />
       <Banner />
+
+      <Divider className="mt-10" />
     </div>
   )
 }
