85ec43cbed34c478c552d4306c523c54219ca0d4
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-06-18 10:42:29 -0300

    Refactor WelcomeSection layout for improved readability and structure

--- src/app/sections/WelcomeSection/index.tsx
+++ src/app/sections/WelcomeSection/index.tsx
@@ -7,50 +7,48 @@ export default function WelcomeSection() {
   const image = Im
 
   return (
-    <div>
-      <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="flex flex-col px-4 lg:px-4 py-4">
-          <SocialMedia />
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="flex flex-col px-4 lg:px-4 py-4">
+        <SocialMedia />
 
-          <div className="flex-1 flex flex-col justify-center text-center lg:text-left">
-            <h2 className="text-4xl lg:text-[46px] xl:text-[58px] font-bold mb-8 leading-tight">
-              <span className="block">Nice to meet you!</span>{' '}
-              <span className="block lg:inline">
-                {' '}
-                I'm{' '}
-                <span className="border-b-4 border-b-blue whitespace-nowrap">
-                  Carlos Oliveira.
-                </span>{' '}
-              </span>
-            </h2>
+        <div className="flex-1 flex flex-col justify-center text-center lg:text-left">
+          <h2 className="text-4xl lg:text-[46px] xl:text-[58px] font-bold mb-8 leading-tight">
+            <span className="block">Nice to meet you!</span>{' '}
+            <span className="block lg:inline">
+              {' '}
+              I'm{' '}
+              <span className="border-b-4 border-b-blue whitespace-nowrap">
+                Carlos Oliveira.
+              </span>{' '}
+            </span>
+          </h2>
 
-            <div className="lg:hidden flex justify-center mb-8 h-80">
-              <Image
-                src={image}
-                alt="Minha imagem"
-                className="rounded-lg object-cover"
-              />
-              x{' '}
-            </div>
+          <div className="lg:hidden flex justify-center mb-8 h-80">
+            <Image
+              src={image}
+              alt="Minha imagem"
+              className="rounded-lg object-cover"
+            />
+            x{' '}
+          </div>
 
-            <p className="text-lg lg:text-xl text-gray-300 mb-12 max-w-lg leading-relaxed mx-auto lg:mx-0">
-              Based in Salvador, Brazil. I'm a front-end developer passionate
-              about crafting accessible and user-friendly web applications.
-            </p>
+          <p className="text-lg lg:text-xl text-gray-300 mb-12 max-w-lg leading-relaxed mx-auto lg:mx-0">
+            Based in Salvador, Brazil. I'm a front-end developer passionate
+            about crafting accessible and user-friendly web applications.
+          </p>
 
-            <div>
-              <Button>contact me</Button>
-            </div>
+          <div>
+            <Button>contact me</Button>
           </div>
         </div>
+      </div>
 
-        <div className="hidden lg:flex mx-auto justify-center items-start">
-          <Image
-            src={image}
-            alt="Minha imagem"
-            className="rounded object-cover"
-          />
-        </div>
+      <div className="hidden lg:flex mx-auto justify-center items-start">
+        <Image
+          src={image}
+          alt="Minha imagem"
+          className="rounded object-cover"
+        />
       </div>
     </div>
   )
