7d211286e4062fe4a32f388bf59ec13d643af550
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-06-18 10:41:37 -0300

    Add Skills section to HomePage for enhanced content display

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,15 @@
 import Divider from '../components/Divider'
+import Skills from '../sections/Skills'
 import WelcomeSection from '../sections/WelcomeSection'
 
 export default function HomePage() {
   return (
-    <div className="bg-gray-700 h-screen px-4">
+    <div className="bg-gray-700 px-4">
       <WelcomeSection />
+
+      <Divider className="mt-10" />
+
+      <Skills />
 
       <Divider className="mt-10" />
     </div>
